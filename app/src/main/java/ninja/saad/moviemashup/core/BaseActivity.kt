8f4b68d0e944bfdf7de566e10283839ebaabee54
-1 <sa.akash0129@gmail.com>
2019-05-10 05:19:47 +0600

    all ui revamp added

---
+++ app/src/main/java/ninja/saad/moviemashup/core/BaseActivity.kt
@@ -0,0 +1,84 @@
+package ninja.saad.moviemashup.core
+
+
+import android.app.ProgressDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.net.ConnectivityManager
+import android.provider.Settings
+import android.widget.Toast
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+
+    lateinit var progressDialog: ProgressDialog
+    lateinit var builder: AlertDialog.Builder
+    lateinit var currentAlertDialog: AlertDialog
+
+    val isOnline: Boolean
+        get() {
+            val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val netInfo = cm.activeNetworkInfo
+            return netInfo != null && netInfo.isConnectedOrConnecting
+        }
+
+    override fun getBaseContext(): Context {
+        return applicationContext
+    }
+
+    fun showLongToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+    }
+
+    fun showProgressDialog(context: Context, title: String, message: String) {
+        progressDialog = ProgressDialog(context)
+        progressDialog.max = 100
+        progressDialog.setTitle(title)
+        progressDialog.setMessage(message)
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+        progressDialog.show()
+        progressDialog.setCancelable(false)
+    }
+
+    fun dismissProgressDialogue(context: Context) {
+        progressDialog.dismiss()
+    }
+
+    fun showAlertDialog(context: Context, title: String, message: String) {
+        builder = AlertDialog.Builder(context)
+
+        builder.setTitle(title)
+        builder.setCancelable(false)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK") { dialogInterface, i ->
+            //((Activity)context).finish();
+        }
+        builder.setNegativeButton("Cancel") { dialogInterface, i ->
+            //((Activity)context).finish();
+        }
+
+        currentAlertDialog = builder.create()
+        currentAlertDialog.show()
+    }
+
+    fun dismissAlertDialog(context: Context) {
+        if (currentAlertDialog.isShowing)
+            currentAlertDialog.dismiss()
+    }
+
+    fun showShortToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun myCurrentDateFormat(): String {
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
+        return dateFormat.format(Date())
+    }
+
+}
--- app/src/main/java/ninja/saad/moviemashup/features/dashboard/MainActivity.kt
+++ app/src/main/java/ninja/saad/moviemashup/features/dashboard/MainActivity.kt
@@ -1,16 +1,29 @@
 package ninja.saad.moviemashup.features.dashboard
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.multidex.MultiDex
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.facebook.common.activitylistener.BaseActivityListener
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import ninja.saad.moviemashup.di.*
@@ -18,7 +31,7 @@ import ninja.saad.moviemashup.util.Navigator
 import kotlinx.android.synthetic.main.activity_main.*
 import ninja.saad.moviemashup.App
 import ninja.saad.moviemashup.R
-import ninja.saad.moviemashup.databinding.ActivityMainBinding
+import ninja.saad.moviemashup.core.BaseActivity
 import ninja.saad.moviemashup.databinding.FragmentMainBinding
 import ninja.saad.moviemashup.features.discover.DiscoverMoviesActivity
 import ninja.saad.moviemashup.features.discover.MovieListAdapter
@@ -27,86 +40,86 @@ import ninja.saad.moviemashup.util.Constant
 import java.util.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var viewModel: MovieListViewModel
-    @Inject
-    lateinit var navigator: Navigator
-
-    //lateinit var appComponent: AppComponent
+class MainActivity : BaseActivity(), GridRecyclerAdapter.RecyclerAdapterOnClickHandler {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MultiDex.install(this)
-        /*(application as App).appComponent
-            .plusContext(ContextModule(this))
-            .inject(this)*/
-        /*Fresco.initialize(this)
-        initAppComponent()*/
-
-        /*val binding = DataBindingUtil.setContentView<FragmentMainBinding>(this,
-            R.layout.fragment_main)*/
 
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter =
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
+        initViews()
+        /*val sectionsPagerAdapter =
             SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+        tabs.setupWithViewPager(viewPager)*/
+        /*val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-        //startActivity(Intent(this, DiscoverMoviesActivity::class.java))
-
-       /* binding.vm = viewModel
-        viewModel.loadMovies(Date())
-        setupRV(binding.rvList)*/
-
-
-       /* setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }*/
     }
+    private fun initViews() {
 
-   /* private fun initAppComponent() {
-        this.appComponent = DaggerAppComponent
-            .builder()
-            .baseModule(BaseModule(this))
-            .networkModule(NetworkModule(Constant.BASE_URL))
-            .build()
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.title = "Movie Mashup"
+        setupRecyclerCardGrid()
     }
 
-    private fun setupRV(rvList: RecyclerView) {
-        rvList.layoutManager = LinearLayoutManager(this)
-        rvList.adapter = MovieListAdapter(viewModel.movies, navigator)
-        rvList.addItemDecoration(
-            DividerItemDecoration(this,
-            DividerItemDecoration.VERTICAL)
-        )
-        rvList.addOnScrollListener(object  : RecyclerView.OnScrollListener(){
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                val lastItem = (rvList.layoutManager as LinearLayoutManager)
-                    .findLastVisibleItemPosition()
-                val totalItem = (rvList.layoutManager as LinearLayoutManager).itemCount
-                if (totalItem - 2 <= lastItem) {
-                    viewModel.loadMovies()
-                }
-            }
-        })
-    }*/
+    private fun setupRecyclerCardGrid() {
+        val mainView = findViewById<RecyclerView>(R.id.dashboard_rv)
+        mainView.setHasFixedSize(true)
+        mainView.layoutManager = GridLayoutManager(this, 2)
+        mainView.adapter = GridRecyclerAdapter(this, DashboardFeatureList.getFeatureListOfDashboard(applicationContext))
+    }
+
+
+    override fun onClick(target: String) {
+        val i = Intent()
+
+        if (target === applicationContext.resources.getString(R.string.feature01)) {
+            i.setClass(this@MainActivity, DiscoverMoviesActivity::class.java)
+            startActivity(i)
+        } else if (target === applicationContext.resources.getString(R.string.feature02)) {
+            i.setClass(this@MainActivity, DiscoverMoviesActivity::class.java)
+            startActivity(i)
+        } else if (target === applicationContext.resources.getString(R.string.feature03)) {
+            i.setClass(this@MainActivity,DiscoverMoviesActivity::class.java)
+            startActivity(i)
+        } else if (target === applicationContext.resources.getString(R.string.feature04)) {
+            i.setClass(this@MainActivity, DiscoverMoviesActivity::class.java)
+            startActivity(i)
+        } else if (target === applicationContext.resources.getString(R.string.feature05)) {
+            showLongToast("To be implemented")
+        } else if (target === applicationContext.resources.getString(R.string.feature06)) {
+            showLongToast("To be implemented")
+        } else {
+        }
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            AlertDialog.Builder(this)
+                .setTitle("Exit Confirmation")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(
+                    android.R.string.no, null
+                )
+                .setPositiveButton(
+                    android.R.string.yes
+
+                ) { _, _ -> super.onBackPressed() }
+                .create().show()
+        }
+    }
+
 }
