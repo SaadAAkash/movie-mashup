2261049d637f9e42e47a43a0b45a0380326a6a35
-1 <sa.akash0129@gmail.com>
2019-05-10 08:57:27 +0600

    updated release 01

--- app/src/main/java/ninja/saad/moviemashup/util/Constant.kt
+++ app/src/main/java/ninja/saad/moviemashup/util/Constant.kt
@@ -1,8 +1,7 @@
 package ninja.saad.moviemashup.util
 
 object Constant {
-    val API_KEY: String = "7702fb313a7de40aa73fe71fd70af328"
+    val API_KEY : String = "dac2426fb73188ce34557d06899a32dc"
     val BASE_URL: String = "https://api.themoviedb.org"
     val IMG_BASE_URL: String = "https://image.tmdb.org/t/p/"
-    val IMG_SIZE_LIST: String = "w185"
 }
